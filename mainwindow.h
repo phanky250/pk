02ca14a7916672244c926ce578a7facf27dc5ac1
phanky <phanky250@gmail.com>
2020-07-09 15:58:34 +0700

    _0

---
+++ mainwindow.h
@@ -0,0 +1,28 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+class MainWindow : public QMainWindow {
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    void createMenus();
+
+protected:
+    void closeEvent(QCloseEvent *event);
+
+private slots:
+    void newFile();
+    void open();
+    bool save();
+    bool saveAs();
+    void find();
+    void goToCell();
+    void sort();
+    void about();
+};
+
+#endif // MAINWINDOW_H
